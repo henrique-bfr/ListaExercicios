fccc2151f5f4d5cbc8b7272d1272995359ddd540
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-02 21:47:41 -0300

    Finalizado ex04

--- Exercicio04.ConsoleApp1/Program.cs
+++ Exercicio04.ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Ex04 - 
+    Crie um programa para calcular o salário total de um vendedor. 
+        Deverá ser informado o salário base e o total de vendas. 
+        A comissão é calculada com um percentual (informado pelousuário) sobre o total de vendas.
+*/
+Console.Write("Digite o seu salário base: ");
+double salarioBase = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o total de vendas: ");
+double vendas = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o percentual de comissão das suas vendas: ");
+double percentual = Convert.ToDouble(Console.ReadLine());
+
+double salarioFinal = salarioBase + ((vendas * percentual) / 100);
+
+Console.WriteLine($"Seu salário total é: {salarioFinal:F2}");
+Console.ReadLine();
