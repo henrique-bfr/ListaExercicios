f55f0d78cc567ceee4b04c21a38c5fc99b0107fb
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-02 23:20:38 -0300

    Finalizado ex06

--- Exercicio06.ConsoleApp/Program.cs
+++ Exercicio06.ConsoleApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 06 - Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor que C.
+
+using System.Threading.Tasks.Dataflow;
+
+Console.Write("Digite o valor de A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor de B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o valor de C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if((a + b) < c)
+{
+    Console.WriteLine("A soma de A + B é MENOR que C");
+}
+else
+{
+    Console.WriteLine("Fim do programa, digite ENTER para sair");
+    Console.ReadLine();
+}
