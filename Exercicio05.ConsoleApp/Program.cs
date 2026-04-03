fc9c1c668a2479f8145251edd68a149bf66630f0
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-02 23:09:33 -0300

    Finalizado ex05

--- Exercicio05.ConsoleApp/Program.cs
+++ Exercicio05.ConsoleApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 05 - Crie um programa para calcular a média ponderada de duas provas realizadas por um aluno
+
+Console.Write("Digite a nota da primeira prova: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o peso da primeira prova: ");
+double peso1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a nota da segunda prova: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o peso da segunda prova: ");
+double peso2 = Convert.ToDouble(Console.ReadLine());
+
+double media = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+
+Console.WriteLine($"Sua média ponderada foi: {media:F2} pontos");
+Console.ReadLine();
