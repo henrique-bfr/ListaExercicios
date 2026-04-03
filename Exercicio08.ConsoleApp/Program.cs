01cac27f79d5998e4e4f6a3955591b0257fab661
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-03 00:05:45 -0300

    Finalizado ex08

--- Exercicio08.ConsoleApp/Program.cs
+++ Exercicio08.ConsoleApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 08. Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+Console.Write("Digite um número inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"O número {numero} é PAR.");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ÍMPAR.");
+}
+
+Console.ReadLine();
