c0b33f0e54e1055136661502c3aa3d1595047399
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-02 21:16:40 -0300

    Finalizado ex03

--- Ex03.ConsoleApp/Program.cs
+++ Ex03.ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex03 - Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+Console.Write("Digite sua temperatura em Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit = (celsius * 1.8) + 32;
+
+Console.WriteLine($"Sua temperatura em Fahrenheit é: {fahrenheit}°F");
