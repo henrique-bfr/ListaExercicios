6e6054725751667ba5e9027a7440ef64a6442ee9
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-05 14:38:04 -0300

    Finalizado ex09

--- Exercicio09.ConsoleApp/Program.cs
+++ Exercicio09.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Digite um valor para calcular o fatorial: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+long resultado = 1;
+Console.Write($"{numero}! = ");
+
+for (int i = numero; i >= 1; i--)
+{
+    resultado *= i;
+    if (i > 1)
+    {
+        Console.Write(i + " X ");
+    }
+    else
+    {
+        Console.Write(i + " = ");
+    }
+}
+
+Console.WriteLine(resultado);
+Console.ReadLine();
