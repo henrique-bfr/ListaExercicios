22c76cf345c057cc7aab34632157c88ddccb2dcc
Henrique Daniel <henriquesouzadaniel@gmail.com>
2026-04-05 15:03:53 -0300

    Finalizado ex10

--- Exercicio10.ConsoleApp/Program.cs
+++ Exercicio10.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Escreva um programa que imprima os números de 1 a 100 em ordem crescente, substituindo
+os números múltiplos de 3 pela palavra "Fizz" e os múltiplos de 5 pela palavra "Buzz". Para
+números que são múltiplos de ambos, use "FizzBuzz".
+*/
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.ReadLine();
